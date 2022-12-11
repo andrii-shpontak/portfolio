8b43d627ad8e8edf881285bb4e90579cb3e1ddf3
AndriiSH <andriishpontak@gmail.com>
2022-12-11 15:05:13 +0200

    added and stylized 4 blocks

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.progress__scale_percent'),
+      lines = document.querySelectorAll('.progress__scale_row');
+
+      counters.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+      });
